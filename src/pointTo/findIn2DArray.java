e74ceca6fbed1004a8257b13880cd5d6f3569add
Chace <chaceliang@gmail.com>
2014-04-21 23:58:15 -0700

    find target in 2d array

--- src/pointTo/findIn2DArray.java
+++ src/pointTo/findIn2DArray.java
@@ -2,12 +2,80 @@ package pointTo;
 
 public class findIn2DArray {
 
+	public static boolean findIn2DArray(int[][] input, int target) {
+		boolean found = false;
+		if (input == null) {
+			return found;
+		}
+		int row = input.length - 1;
+		int col = input[0].length - 1;
+		if (row < 0 || col < 0) {
+			return found;
+		}
+		int s = 0, e = col;
+		while (s < e) {
+			if (s == e && input[row][s] != target) {
+				break;
+			}
+			int mid = (s + e) / 2;
+			if (input[row][mid] == target) {
+				found = true;
+				return found;
+			} else if (input[row][mid] > target) {
+				e = mid;
+			} else {
+				s = mid + 1;
+			}
+		}
+		col = s;
+		s = 0;
+		e = row;
+		while (s <= e) {
+			if (s == e && input[s][col] != target) {
+				break;
+			}
+			int mid = (s + e) / 2;
+			if (input[mid][col] == target) {
+				found = true;
+				return found;
+			} else if (input[mid][col] > target) {
+				e = mid;
+			} else {
+				s = mid + 1;
+			}
+		}
+		return found;
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] input1 = {
+				{1,2,8,9},
+				{2,4,9,12},
+				{4,7,10,13},
+				{6,8,11,15}
+		};
+		int[][] input2 = {
+				{1,2,8,9}
+		};
+		int[][] input3 = {
+				{1},
+				{2},
+				{4},
+				{6}
+		};
+		for (int i = 0; i < 20; i++) {
+			System.out.println(i + ": " + findIn2DArray(input1, i));
+		}
+		System.out.println();
+		for (int i = 0; i < 20; i++) {
+			System.out.println(i + ": " + findIn2DArray(input2, i));
+		}
+		System.out.println();
+		for (int i = 0; i < 20; i++) {
+			System.out.println(i + ": " + findIn2DArray(input3, i));
+		}
 	}
 
 }
