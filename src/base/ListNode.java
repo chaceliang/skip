b93e816ff64d3eb0dc38ef2822f1a62415b0ac11
Chace <chaceliang@gmail.com>
2014-04-26 15:04:03 -0700

    Add more helper function

--- src/base/ListNode.java
+++ src/base/ListNode.java
@@ -18,4 +18,27 @@ public class ListNode {
 		head1.next.next.next.next.next = new ListNode(0);
 		return head1;
 	}
+	
+	public static ListNode giveMeAList(int[] value) {
+		ListNode head = null;
+		ListNode curr = null;
+		for (int i = 0; i < value.length; i++) {
+			if (curr == null) {
+				curr = new ListNode(value[i]);
+				head = curr;
+			} else {
+				curr.next = new ListNode(value[i]);
+				curr = curr.next;
+			}
+		}
+		return head;
+	}
+	
+	public static void printList(ListNode head) {
+		while (head != null) {
+			System.out.print(head.value + " ");
+			head = head.next;
+		}
+		System.out.println();
+	}
 }
