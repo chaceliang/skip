c6d5db91feeec36ca6648f0cf2d16c5982f1c64c
Chace <chaceliang@gmail.com>
2014-04-26 15:21:01 -0700

    Does tree1 has tree2

---
+++ src/pointTo/Question18DoesTree1HasTree2.java
@@ -0,0 +1,28 @@
+package pointTo;
+
+import base.TreeNode;
+
+public class Question18DoesTree1HasTree2 {
+	
+	public static boolean doesTree1HasTree2(TreeNode node1, TreeNode node2) {
+		if (node2 == null) {
+			return true;
+		}
+		if (node1 == null) {
+			return false;
+		}
+		if (node1.value != node2.value) {
+			return false;
+		}
+		return doesTree1HasTree2(node1.left, node2.left) && doesTree1HasTree2(node1.right, node2.right);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
