2ecd04f45a01d1f7018bdb1cd444238619243627
Chace <chaceliang@gmail.com>
2014-04-26 14:44:39 -0700

    Add helper function to return a list

--- src/base/ListNode.java
+++ src/base/ListNode.java
@@ -8,4 +8,14 @@ public class ListNode {
 		this.value = v;
 		next = null;
 	}
+	
+	public static ListNode giveMeAList() {
+		ListNode head1 = new ListNode(5);
+		head1.next = new ListNode(4);
+		head1.next.next = new ListNode(3);
+		head1.next.next.next = new ListNode(2);
+		head1.next.next.next.next = new ListNode(1);
+		head1.next.next.next.next.next = new ListNode(0);
+		return head1;
+	}
 }
