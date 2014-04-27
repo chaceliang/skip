27201fd6a44129d7eb894fa183a4f49a667eb35a
Chace <chaceliang@gmail.com>
2014-04-26 22:07:52 -0700

    Print tree by level order

---
+++ src/pointTo/Question23PrintTreeByLevel.java
@@ -0,0 +1,39 @@
+package pointTo;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import base.TreeNode;
+
+public class Question23PrintTreeByLevel {
+
+	public static void printTreeByLevel(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+		Queue<TreeNode> container = new LinkedList<TreeNode>();
+		container.add(node);
+		while (!container.isEmpty()) {
+			int size = container.size();
+			for (int i = 0; i < size; i++) {
+				TreeNode n = container.poll();
+				System.out.print(n.value + " ");
+				if (n.left != null) {
+					container.add(n.left);
+				}
+				if (n.right != null) {
+					container.add(n.right);
+				}
+			}
+			System.out.println();
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
