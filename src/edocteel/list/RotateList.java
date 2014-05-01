31d67a91596c5444a750a01912c25f9bdcce7c85
Chace <chaceliang@gmail.com>
2014-05-01 11:24:54 -0700

    Rotate List

---
+++ src/edocteel/list/RotateList.java
@@ -0,0 +1,44 @@
+package edocteel.list;
+
+import base.ListNode;
+
+public class RotateList {
+	
+	public static ListNode rotateRight(ListNode head, int n) {
+		if (head == null) {
+			return head;
+		}
+		int length = 1;
+		ListNode last = head;
+		while (last.next != null) {
+			last = last.next;
+			length++;
+		}
+		n = n % length;
+		if (n == 0) {
+			return head;
+		}
+		int steps = length - n;
+		ListNode start = new ListNode(0);
+		start.next = head;
+		while (steps > 0) {
+			start = start.next;
+			steps--;
+		}
+		ListNode ret = start.next;
+		start.next = null;
+		last.next = head;
+		return ret;
+    }
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		rotateRight(head, 1);
+	}
+
+}
