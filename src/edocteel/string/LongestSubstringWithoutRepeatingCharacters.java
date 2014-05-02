8e9627572845a86ed5cdf70c9c34f077a260378a
Chace <chaceliang@gmail.com>
2014-05-02 00:36:45 -0700

    Longest substring without repeating characters

---
+++ src/edocteel/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,40 @@
+package edocteel.string;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+	public int lengthOfLongestSubstring(String s) {
+		if (s == null || s.length() <= 0) {
+			return 0;
+		}
+		int i = 0, j = 0;
+		int result = 0;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		while (j < s.length()) {
+			Character c = s.charAt(j);
+			if (map.containsKey(c)) {
+				int length = j - i;
+				if (result < length) {
+					result = length;
+				}
+				Integer index = map.get(c);
+				i = Math.max(i, index+1);
+			}
+			map.put(c, j);
+			j++;
+		}
+		if (result < j - i) {
+			return j - i;
+		} 
+		return result;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
