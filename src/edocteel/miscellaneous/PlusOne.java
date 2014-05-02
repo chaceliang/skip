a07c6b6fa7c1fee74ab580ae2efd13b7d9f9e41c
Chace <chaceliang@gmail.com>
2014-05-02 10:46:45 -0700

    Plus one

--- src/edocteel/miscellaneous/PlusOne.java
+++ src/edocteel/miscellaneous/PlusOne.java
@@ -12,6 +12,7 @@ public class PlusOne {
 		for (int i = digits.length - 1; i >= 0; i--) {
 			bit = (digits[i] + carry) % 10;
 			carry = (digits[i] + carry) / 10;
+			digits[i] = bit;
 			if (carry == 0) {
 				needNewArray = false;
 				break;
