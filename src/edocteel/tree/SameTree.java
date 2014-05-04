250762c0c70492b4c31724a42532f1612f5c919a
Chace <chaceliang@gmail.com>
2014-05-04 00:05:25 -0700

    Same tree

---
+++ src/edocteel/tree/SameTree.java
@@ -0,0 +1,22 @@
+package edocteel.tree;
+
+import base.TreeNode;
+
+/**
+ * Created by chace on 5/4/14.
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            if (p == null && q == null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
