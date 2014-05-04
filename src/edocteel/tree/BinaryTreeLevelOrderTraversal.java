f8d469f755358aaddec762f795df10b9fcfe4a85
Chace <chaceliang@gmail.com>
2014-05-03 22:00:52 -0700

    Binary tree level order traversal

---
+++ src/edocteel/tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,44 @@
+package edocteel.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import base.TreeNode;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> tmp = new LinkedList<TreeNode>();
+        tmp.add(root);
+        while (!tmp.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            int size = tmp.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = tmp.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    tmp.add(node.left);
+                }
+                if (node.right != null) {
+                    tmp.add(node.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
