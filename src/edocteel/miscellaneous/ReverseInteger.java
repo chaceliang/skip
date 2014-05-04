43b897a51718c59328f0386cf4770ed84f48666d
Chace <chaceliang@gmail.com>
2014-05-04 00:36:29 -0700

    reverse integer

--- src/edocteel/miscellaneous/ReverseInteger.java
+++ src/edocteel/miscellaneous/ReverseInteger.java
@@ -4,4 +4,20 @@ package edocteel.miscellaneous;
  * Created by chace on 5/4/14.
  */
 public class ReverseInteger {
+    public int reverse(int x) {
+        boolean isNegative = false;
+        if (x < 0) {
+            isNegative = true;
+            x *= -1;
+        }
+        int result = 0;
+        while (x > 0) {
+            result = result * 10 + x % 10;
+            x /= 10;
+        }
+        if (isNegative) {
+            result *= -1;
+        }
+        return result;
+    }
 }
