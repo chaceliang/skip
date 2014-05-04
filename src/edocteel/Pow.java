377ecdfa37ae9eb80a2f08acdfe0886bfc953a7f
Chace <chaceliang@gmail.com>
2014-05-04 00:27:51 -0700

    pow

---
+++ src/edocteel/Pow.java
@@ -0,0 +1,25 @@
+package edocteel;
+
+/**
+ * Created by chace on 5/4/14.
+ */
+public class Pow {
+    public double pow(double x, int n) {
+        double result = 1;
+        if (n < 0) {
+            x = 1/x;
+            n = -n;
+        }
+        while (n > 0) {
+            if (n % 2 == 0) {
+                x *= x;
+                n /= 2;
+            } else {
+                result *= x;
+                n--;
+            }
+        }
+
+        return result;
+    }
+}
