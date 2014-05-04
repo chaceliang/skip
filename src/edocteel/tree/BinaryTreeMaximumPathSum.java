fca161da65ef63968f843c53d00e0177193ecc8b
Chace <chaceliang@gmail.com>
2014-05-03 23:27:03 -0700

    Maximum path sum

--- src/edocteel/tree/BinaryTreeMaximumPathSum.java
+++ src/edocteel/tree/BinaryTreeMaximumPathSum.java
@@ -1,7 +1,38 @@
 package edocteel.tree;
 
+import base.TreeNode;
+
 /**
  * Created by chace on 5/3/14.
  */
 public class BinaryTreeMaximumPathSum {
+    static int maxSoFar = 0;
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        maxSoFar = root.val;
+        helper(root);
+        return maxSoFar;
+    }
+
+    public int helper(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftMax = helper(node.left);
+        int rightMax = helper(node.right);
+        int maxPath = node.val;
+        if (leftMax > 0) {
+            maxPath += leftMax;
+        }
+        if (rightMax > 0) {
+            maxPath += rightMax;
+        }
+        if (maxPath > maxSoFar) {
+            maxSoFar = maxPath;
+        }
+        int result = node.val;
+        return Math.max(result, result + Math.max(leftMax, rightMax));
+    }
 }
