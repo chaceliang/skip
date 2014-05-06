6a466e9e88397c7d1611565a02f81cd1bfc32485
Chace <chaceliang@gmail.com>
2014-05-05 23:03:54 -0700

    Generate Parentheses

---
+++ src/edocteel/dp/GenerateParentheses.java
@@ -0,0 +1,30 @@
+package edocteel.dp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by chace on 5/5/14.
+ */
+public class GenerateParentheses {
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> results = new ArrayList<String>();
+        if (n <= 0) {
+            return results;
+        }
+        helper(results, n, n, 0, "");
+        return results;
+    }
+
+    public void helper(ArrayList<String> results, int left, int right, int sum, String result) {
+        if (left == 0 && right == 0 && sum == 0) {
+            results.add(result);
+        } else if (left < 0 || right < 0 || sum < 0) {
+            return;
+        } else {
+            helper(results, left-1, right, sum+1, result + "(");
+            if (sum > 0) {
+                helper(results, left, right-1, sum-1, result + ")");
+            }
+        }
+    }
+}
