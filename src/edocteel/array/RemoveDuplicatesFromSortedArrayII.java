7321abdbfd240cdff12b0be48aef973c8b528c05
Chace <chaceliang@gmail.com>
2014-05-05 21:29:04 -0700

    Remove Duplicates from sorted array

---
+++ src/edocteel/array/RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,36 @@
+package edocteel.array;
+
+/**
+ * Created by chace on 5/5/14.
+ */
+public class RemoveDuplicatesFromSortedArrayII {
+    public int removeDuplicates(int[] A) {
+        if (A == null) {
+            return 0;
+        }
+        int length = A.length;
+        if (length < 3) {
+            return length;
+        }
+        int slow = 0, fast = 1, index = 0;
+        while (fast < length) {
+            while (fast < length && A[slow] == A[fast]) {
+                fast++;
+            }
+            if (fast - slow <= 2) {
+                while (fast < slow) {
+                    A[index++] = A[slow++];
+                }
+            } else {
+                A[index++] = A[slow++];
+                A[index++] = A[slow++];
+                slow = fast;
+            }
+            fast++;
+        }
+        while (slow < length) {
+            A[index++] = A[slow++];
+        }
+        return index;
+    }
+}
