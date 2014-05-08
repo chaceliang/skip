93322194b1284e3c9c7011b1b4dca33a800204b9
Chace <chaceliang@gmail.com>
2014-05-07 23:14:05 -0700

    ConstructBinaryTreeFromInorderAndPostorderTraversal.java

---
+++ src/edocteel/dp/LongestConsecutiveSequence.java
@@ -0,0 +1,7 @@
+package edocteel.dp;
+
+/**
+ * Created by chace on 5/7/14.
+ */
+public class LongestConsecutiveSequence {
+}
--- src/edocteel/tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/edocteel/tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -1,7 +1,17 @@
 package edocteel.tree;
 
+import base.TreeNode;
+
 /**
  * Created by chace on 5/5/14.
  */
 public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return null;
+    }
+
+    public TreeNode helper(int[] inorder, int[] postorder, int inorderIndex, int postorderIndex) {
+        return null;
+    }
 }
