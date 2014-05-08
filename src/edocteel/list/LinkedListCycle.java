e8dc3aa9799ade08589149d8f435c8028faa5d26
Chace <chaceliang@gmail.com>
2014-05-08 00:15:38 -0700

    linked list cycle

--- src/edocteel/list/LinkedListCycle.java
+++ src/edocteel/list/LinkedListCycle.java
@@ -1,7 +1,25 @@
 package edocteel.list;
 
+import base.ListNode;
+
 /**
  * Created by chace on 5/7/14.
  */
 public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head.next, fast = head.next.next;
+        while (fast != null && fast.val != slow.val) {
+            slow = slow.next;
+            fast = fast.next == null ? fast.next : fast.next.next;
+        }
+        if (fast == null) {
+            return false;
+        } else if (fast.val == slow.val) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/edocteel/list/LinkedListCycleII.java
+++ src/edocteel/list/LinkedListCycleII.java
@@ -1,7 +1,50 @@
 package edocteel.list;
 
+import base.ListNode;
+
 /**
  * Created by chace on 5/7/14.
  */
 public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head.next, fast = head.next.next;
+        while (fast != null && fast.val != slow.val) {
+            slow = slow.next;
+            fast = fast.next == null ? fast.next : fast.next.next;
+        }
+        if (fast == null) {
+            return null;
+        } else if (fast.val == slow.val) {
+            slow = head;
+            while (fast.val != slow.val) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return fast;
+        }
+        return null;
+    }
+
+    public ListNode abc(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode fast = head, slow = head;
+        do {
+            if (fast.next != null && fast.next.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            } else {
+                return null;
+            }
+        } while (fast != slow);
+        slow = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
 }
