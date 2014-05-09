00b776a0654ada70a89ed512e506e7b3f16a6808
Chace <chaceliang@gmail.com>
2014-05-08 23:50:02 -0700

    subsets

---
+++ src/edocteel/miscellaneous/Subsets.java
@@ -0,0 +1,24 @@
+package edocteel.miscellaneous;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by chace on 5/8/14.
+ */
+public class Subsets {
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
+        results.add(new ArrayList<Integer>());
+        Arrays.sort(S);
+        for (int i = 0; i < S.length; i++) {
+            int size = results.size();
+            for (int j = 0; j < size; j++) {
+                ArrayList<Integer> result = new ArrayList<Integer>(results.get(j));
+                result.add(S[i]);
+                results.add(result);
+            }
+        }
+        return results;
+    }
+}
