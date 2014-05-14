9e763703ae14c643db8f37be876930f0b15f0142
Chace <chaceliang@gmail.com>
2014-05-14 14:19:56 -0700

    Edit distance

---
+++ src/edocteel/dp/EditDistance.java
@@ -0,0 +1,26 @@
+package edocteel.dp;
+
+/**
+ * Created by chace on 5/14/14.
+ */
+public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        if (word1 == null || word2 == null) {
+            return -1;
+        }
+        int l1 = word1.length(), l2 = word2.length();
+        int[][] dp = new int[l1+1][l2+1];
+        for (int i = 0; i <= l1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= l2; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1] + (word1.charAt(i-1) == word2.charAt(j-1) ? 0 : 1));
+            }
+        }
+        return dp[l1][l2];
+    }
+}
