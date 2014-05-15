71b599b325e71c60a7d50aaed6f7158a576f46e3
Chace <chaceliang@gmail.com>
2014-05-14 17:53:31 -0700

    ConstructBinaryTreeFromPreOrderAndInorderTraversal

---
+++ src/edocteel/tree/ConstructBinaryTreeFromPreOrderAndInorderTraversal.java
@@ -0,0 +1,27 @@
+package edocteel.tree;
+
+import base.TreeNode;
+
+/**
+ * Created by chace on 5/14/14.
+ */
+public class ConstructBinaryTreeFromPreOrderAndInorderTraversal {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+    }
+
+    public TreeNode helper(int[] preorder, int ps, int pe, int[] inorder, int is, int ie) {
+        if (ps > pe || is > ie) {
+            return null;
+        }
+        int rootValue = preorder[ps];
+        TreeNode root = new TreeNode(rootValue);
+        for (int i = is; i <= ie; i++) {
+            if (inorder[i] == rootValue) {
+                root.left = helper(preorder, ps+1, ps+i, inorder, is, i-1);
+                root.right = helper(preorder, ps+i-is+1, pe, inorder, i+1, ie);
+            }
+        }
+        return root;
+    }
+}
