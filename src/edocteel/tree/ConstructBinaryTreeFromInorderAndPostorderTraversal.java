5af2f8f410a25610e71c57d9ccd69298a7550900
Chace <chaceliang@gmail.com>
2014-05-14 17:53:42 -0700

    ConstructBinaryTreeFromInorderAndPostorderTraversal

--- src/edocteel/tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/edocteel/tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -8,10 +8,21 @@ import base.TreeNode;
 public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return null;
+        return helper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
 
-    public TreeNode helper(int[] inorder, int[] postorder, int inorderIndex, int postorderIndex) {
-        return null;
+    public TreeNode helper(int[] inorder, int is, int ie, int[] postorder, int ps, int pe) {
+        if (is > ie || ps > pe) {
+            return null;
+        }
+        int rootValue = postorder[pe];
+        TreeNode root = new TreeNode(rootValue);
+        for (int i = is; i <= ie; i++) {
+            if (inorder[i] == rootValue) {
+                root.left = helper(inorder, is, i-1, postorder, ps, ps+i-is-1);
+                root.right = helper(inorder, i+1, ie, postorder, pe-(ie-i), pe-1);
+            }
+        }
+        return root;
     }
 }
