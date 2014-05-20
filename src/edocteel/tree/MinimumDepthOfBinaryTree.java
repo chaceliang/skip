2d1d73044d63039f999535c04d223f55486e4905
Chace <chaceliang@gmail.com>
2014-05-19 22:27:47 -0700

    clean up code

--- src/edocteel/tree/MinimumDepthOfBinaryTree.java
+++ src/edocteel/tree/MinimumDepthOfBinaryTree.java
@@ -10,18 +10,12 @@ public class MinimumDepthOfBinaryTree {
         if (root == null) {
             return 0;
         }
-        if (root.left == null && root.right == null) {
-            return 1;
+        if (root.left != null && root.right != null) {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        } else if (root.left != null) {
+            return minDepth(root.left) + 1;
+        } else {
+            return minDepth(root.right) + 1;
         }
-
-        int leftHeight = Integer.MAX_VALUE;
-        if (root.left != null) {
-            leftHeight = minDepth(root.left);
-        }
-        int rightHeight = Integer.MAX_VALUE;
-        if (root.right != null) {
-            rightHeight = minDepth(root.right);
-        }
-        return Math.min(leftHeight, rightHeight) + 1;
     }
 }
