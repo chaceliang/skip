95b07025aeb3cf69576194d3982a0b4fcd42f343
Chace <chaceliang@gmail.com>
2014-05-20 19:21:59 -0700

    Clean up code

--- src/edocteel/dp/EditDistance.java
+++ src/edocteel/dp/EditDistance.java
@@ -18,7 +18,8 @@ public class EditDistance {
         }
         for (int i = 1; i <= l1; i++) {
             for (int j = 1; j <= l2; j++) {
-                dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1] + (word1.charAt(i-1) == word2.charAt(j-1) ? 0 : 1));
+                dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]),
+                        dp[i-1][j-1] + (word1.charAt(i-1) == word2.charAt(j-1) ? 0 : 1));
             }
         }
         return dp[l1][l2];
