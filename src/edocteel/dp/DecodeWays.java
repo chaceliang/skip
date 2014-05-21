f277fe7900fb42a1f079bf6421bf6d0675cb51af
Chace <chaceliang@gmail.com>
2014-05-20 19:49:25 -0700

    clean up

--- src/edocteel/dp/DecodeWays.java
+++ src/edocteel/dp/DecodeWays.java
@@ -20,7 +20,7 @@ public class DecodeWays {
         int[] dp = new int[s.length()+1];
         dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i < s.length() + 1; i++) {
+        for (int i = 2; i <= s.length(); i++) {
             if (s.charAt(i-1) == '0') {
                 dp[i] = dp[i-2];
             } else if (s.charAt(i-2) == '1' || (s.charAt(i-2) == '2' && s.charAt(i-1) <= '6')) {
