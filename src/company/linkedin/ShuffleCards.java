c0f3f424f1d5a064cd4cbcc55cbbbafc74e954e2
Chace <chaceliang@gmail.com>
2014-05-25 14:07:58 -0700

    Add Linkedin

---
+++ src/company/linkedin/ShuffleCards.java
@@ -0,0 +1,33 @@
+package company.linkedin;
+
+import java.util.Random;
+
+/**
+ * Created by chace on 5/25/14.
+ */
+public class ShuffleCards {
+    public void shuffle() {
+        int size = 52;
+        int[] cards = new int[size];
+        for (int i = 0; i < size; i++) {
+            cards[i] = i;
+        }
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            int r = rand.nextInt(size - i);
+            int t = cards[r];
+            cards[r] = cards[i];
+            cards[i] = t;
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(cards[i] + 1);
+            System.out.print(" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ShuffleCards test = new ShuffleCards();
+        test.shuffle();
+    }
+}
