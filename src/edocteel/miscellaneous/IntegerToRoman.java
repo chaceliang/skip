349e476ad3fb3fa3e5509cb0c5f463949c8572f6
Chace <chaceliang@gmail.com>
2014-05-26 15:26:17 -0700

    Integer to roman

---
+++ src/edocteel/miscellaneous/IntegerToRoman.java
@@ -0,0 +1,31 @@
+package edocteel.miscellaneous;
+
+/**
+ * Created by chace on 5/26/14.
+ */
+public class IntegerToRoman {
+
+    public String intToRoman(int num) {
+        String[] ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII",
+                "IX" };
+        String[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX",
+                "XC" };
+        String[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC",
+                "DCCC", "CM" };
+        String[] thousands = { "", "M", "MM", "MMM" };
+        if (num >= 4000 || num <= 0) {
+            return null;
+        }
+        StringBuilder roman = new StringBuilder();
+
+        roman.append(thousands[num/1000]);
+        num = num % 1000;
+        roman.append(hundreds[num/100]);
+        num = num % 100;
+        roman.append(tens[num/10]);
+        num = num % 10;
+        roman.append(ones[num]);
+
+        return roman.toString();
+    }
+}
