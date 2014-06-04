8e2946e1da0a17a2a2ade839696ead4c47e83a82
Chace <chaceliang@gmail.com>
2014-06-04 16:12:53 -0700

    Maximum subarray

--- src/edocteel/miscellaneous/MaximumSubarray.java
+++ src/edocteel/miscellaneous/MaximumSubarray.java
@@ -10,15 +10,42 @@ public class MaximumSubarray {
         }
         int max = Integer.MIN_VALUE;
         int sum = 0;
+
+        int maxNeg = Integer.MIN_VALUE;
+        boolean isAllNeg = true;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > 0) {
+                isAllNeg = false;
+            } else {
+                if (A[i] > maxNeg) {
+                    maxNeg = A[i];
+                }
+            }
+        }
+        if (isAllNeg) {
+            return maxNeg;
+        }
         for (int i = 0; i < A.length; i++) {
             sum += A[i];
-            if (sum > max) {
-                max = sum;
-            }
             if (sum < 0) {
                 sum = 0;
+            } else if (sum > max) {
+                max = sum;
             }
         }
         return max;
+    }
+
+    public int maxSubArrayDP(int[] A) {
+        if (A == null || A.length <= 0) {
+            return 0;
+        }
+        int maxSoFar = A[0], i;
+        int curMax = A[0];
+        for (i = 1; i < A.length; i++) {
+            curMax = Math.max(A[i], curMax + A[i]);
+            maxSoFar = Math.max(maxSoFar, curMax);
+        }
+        return maxSoFar;
     }
 }
