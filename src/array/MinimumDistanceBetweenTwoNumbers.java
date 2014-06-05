21f0e685fdcd270e2cc71f970a5d54156c20604b
Chace <chaceliang@gmail.com>
2014-06-04 20:47:22 -0700

    min distance of two words

--- src/array/MinimumDistanceBetweenTwoNumbers.java
+++ src/array/MinimumDistanceBetweenTwoNumbers.java
@@ -26,6 +26,27 @@ public class MinimumDistanceBetweenTwoNumbers {
         return min;
     }
 
+    public static int minDistance(String[] words, String word1, String word2) {
+        if (words == null || words.length <= 0) {
+            return -1;
+        }
+        int prev = -1;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1) || words[i].equals(word2)) {
+                if (prev == -1) {
+                    prev = i;
+                } else {
+                    if (!words[prev].equals(words[i]) && min > i - prev) {
+                        min = i - prev;
+                    }
+                    prev = i;
+                }
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         int arr[] ={3, 5, 4, 2, 6, 3, 0, 0, 5, 4, 8, 3};
         System.out.println(minimumDistance(arr, 3, 6));
