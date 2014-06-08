58482a2566ca9e299c5311b1d41c055965cf25db
Chace <chaceliang@gmail.com>
2014-06-07 23:59:14 -0700

    一个数组,一个target,求所有的pairs, array[i] - array[j] = k.

---
+++ src/array/TargetDiff.java
@@ -0,0 +1,52 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by chace on 6/7/14.
+ */
+public class TargetDiff {
+    // 一个数组,一个target,求所有的pairs, array[i] - array[j] = k.
+
+    public static ArrayList<Pair> cal(int[] A, int k) {
+        ArrayList<Pair> results = new ArrayList<Pair>();
+        if (A == null) {
+            throw new NullPointerException();
+        }
+
+        if (A.length < 2) {
+            return results;
+        }
+        Arrays.sort(A);
+        for (int i = 0, j = 1; j < A.length;) {
+            if (A[j] - A[i] == k) {
+                Pair tmp = new Pair(i, j);
+                results.add(tmp);
+                j++;
+            } else if (A[j] - A[i] > k) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Pair> results = cal(new int[]{1,2,3,4,5}, 2);
+        for (Pair p : results) {
+            System.out.println(p.i + ", " + p.j);
+        }
+    }
+}
+
+class Pair {
+    int i;
+    int j;
+    public Pair(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+}
