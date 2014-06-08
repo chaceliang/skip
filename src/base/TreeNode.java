1238fe2c2992ef0d61617ed1f24bc2ba7c8b7b04
Chace <chaceliang@gmail.com>
2014-06-08 16:19:36 -0700

    Find next node in bt with or without parent pointer

--- src/base/TreeNode.java
+++ src/base/TreeNode.java
@@ -5,10 +5,12 @@ public class TreeNode {
 	public int val;
 	public TreeNode left;
 	public TreeNode right;
+    public TreeNode parent;
 	
 	public TreeNode(int v) {
 		this.val = v;
 		this.left = null;
 		this.right = null;
+        this.parent = null;
 	}
 }
---
+++ src/tree/GetNext.java
@@ -0,0 +1,57 @@
+package tree;
+
+import base.TreeNode;
+
+/**
+ * Created by chace on 6/8/14.
+ */
+public class GetNext {
+
+    public TreeNode getMin(TreeNode n) {
+        TreeNode curr = n;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+
+    public TreeNode getNext(TreeNode root, TreeNode n) {
+        if (n == null) {
+            throw new NullPointerException();
+        }
+        if (n.right != null) {
+            return getMin(n.right);
+        }
+
+        TreeNode parent = root;
+        TreeNode curr = root;
+        while (curr != n) {
+            parent = curr;
+            if (curr.val > n.val) {
+                curr = curr.left;
+            } else if (curr.val < n.val) {
+                curr = curr.right;
+            }
+        }
+        return parent;
+    }
+
+    // With parent pointer
+    public TreeNode getNext(TreeNode n) {
+        if (n == null) {
+            throw new NullPointerException();
+        }
+        if (n.right != null) {
+            return getMin(n.right);
+        }
+
+        TreeNode tmp = n;
+        while (tmp.parent != null) {
+            if (tmp == tmp.parent.left) {
+                return tmp.parent;
+            }
+            tmp = tmp.parent;
+        }
+        return null;
+    }
+}
