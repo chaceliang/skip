3a866038bd71af830c91c65f68d97c9e70324913
Chace <chaceliang@gmail.com>
2014-06-09 21:14:11 -0700

    trim multiple space into one space

---
+++ src/string/ReplaceSpace.java
@@ -0,0 +1,45 @@
+package string;
+
+/**
+ * Created by chace on 6/9/14.
+ */
+public class ReplaceSpace {
+
+    public static String replaceSpace(String input) {
+        if (input == null) {
+            throw new NullPointerException();
+        }
+        int i = 0, j = 0, len = input.length();
+        char[] tmp = input.toCharArray();
+
+        while (j < len) {
+            while (j < len && tmp[j] == ' ') {
+                j++;
+            }
+            if (j == len) {
+                break;
+            }
+            while (j < len && tmp[j] != ' ') {
+                swap(tmp, i, j);
+                i++; j++;
+            }
+            if (j == len) {
+                break;
+            }
+            swap(tmp, i, j);
+            i++;
+            j++;
+        }
+        return new String(tmp);
+    }
+
+    public static void swap(char[] A, int i, int j) {
+        char t = A[i];
+        A[i] = A[j];
+        A[j] = t;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(replaceSpace("Hello   World        yoyo "));
+    }
+}
