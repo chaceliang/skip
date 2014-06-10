9e4285f0df46423a2d19bb8adb69a59e77eaac77
Chace <chaceliang@gmail.com>
2014-06-09 23:20:05 -0700

    rename

--- src/array/SearchIn2DArray2.java
+++ src/array/SearchIn2DArray2.java
@@ -3,7 +3,7 @@ package array;
 /**
  * Created by chace on 6/6/14.
  */
-public class SearchIn2DArry2 {
+public class SearchIn2DArray2 {
 
     //
     //一个m,n 矩阵 每一行升序排列，每一列同样升序排列。要求找到一个数字是否在矩阵中，注意并不保证每一行头元素高于上一行末元素。
