9cbde9f1e13949a1fe98800e0997b12a8e6819da
Chace <chaceliang@gmail.com>
2014-06-09 19:53:04 -0700

    Add faster can jump II

--- src/edocteel/dp/JumpGameII.java
+++ src/edocteel/dp/JumpGameII.java
@@ -4,6 +4,21 @@ package edocteel.dp;
  * Created by chace on 5/10/14.
  */
 public class JumpGameII {
+    public int jumpFast(int[] A) {
+        int steps = 0;
+        for (int i = 0, max = 0, next = 0; i < A.length - 1; i++) {
+            max = Math.max(A[i]+i, max);
+            if (i == next) {
+                if (max == next) {
+                    return -1;
+                }
+                next = max;
+                steps++;
+            }
+        }
+        return steps;
+    }
+
     public int jump(int[] A) {
         int start = 0, end = 0, count = 0;
         if (A == null || A.length <= 1) {
