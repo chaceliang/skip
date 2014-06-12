5786e2f2446cd49d0b96538b750e5332d3cd6908
Chace <chaceliang@gmail.com>
2014-06-11 23:38:12 -0700

    cleaner solution....

--- src/edocteel/miscellaneous/MaximumSubarray.java
+++ src/edocteel/miscellaneous/MaximumSubarray.java
@@ -4,7 +4,26 @@ package edocteel.miscellaneous;
  * Created by chace on 5/9/14.
  */
 public class MaximumSubarray {
+
     public int maxSubArray(int[] A) {
+        if (A == null) {
+            throw new NullPointerException();
+        }
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+
+    public int maxSubArray1(int[] A) {
         if (A == null || A.length <= 0) {
             return 0;
         }
