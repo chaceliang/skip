748160d36adb75d37cad4a1015d4cc15d0f0bfb3
Chace <chaceliang@gmail.com>
2014-06-11 17:51:02 -0700

    find rotated position dup or no-dup

---
+++ src/array/FindRotatedPosition.java
@@ -0,0 +1,43 @@
+package array;
+
+/**
+ * Created by chace on 6/11/14.
+ */
+public class FindRotatedPosition {
+
+    // No-duplicate
+    public static int findRotatedPosition(int[] A) {
+        if (A == null) {
+            throw new NullPointerException();
+        }
+        int s = 0, e = A.length - 1;
+        while (s < e) {
+            if (e - s == 1) {
+                return A[s] > A[e] ? A[e] : A[s];
+            }
+            int mid = (s + e) / 2;
+            if (A[mid - 1] > A[mid] || A[mid] > A[mid + 1]) {
+                if (A[mid - 1] > A[mid]) {
+                    return A[mid];
+                } else {
+                    return A[mid + 1];
+                }
+            } else if (A[mid] < A[e]) {
+                e = mid;
+            } else {
+                s = mid;
+            }
+        }
+        return A[s];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findRotatedPosition(new int[] {1,2,3,4,5,5,6,7}));
+        System.out.println(findRotatedPosition(new int[] {4,4,4,5,6,6,7,7,7,1,1,1,1,2,3}));
+        System.out.println(findRotatedPosition(new int[] {1,2}));
+        System.out.println(findRotatedPosition(new int[] {2,1}));
+        System.out.println(findRotatedPosition(new int[] {2,3,1}));
+        System.out.println(findRotatedPosition(new int[] {1}));
+
+    }
+}
