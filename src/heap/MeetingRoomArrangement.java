59ba4d98cd4a17a2deb5784ea3d82b12f9627ec0
Chace <chaceliang@gmail.com>
2014-06-11 23:42:09 -0700

    fix bug

--- src/heap/MeetingRoomArrangement.java
+++ src/heap/MeetingRoomArrangement.java
@@ -64,6 +64,6 @@ class Meeting implements Comparable<Meeting> {
 class MeetingComparator implements Comparator<Meeting> {
     @Override
     public int compare(Meeting meeting1, Meeting meeting2) {
-        return (int)(meeting1.end = meeting2.end);
+        return (int)(meeting1.end - meeting2.end);
     }
 }
