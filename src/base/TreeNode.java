8f465b1ec4c81bf83758c2e6c59cd68dfe6c3cdf
Chace <chaceliang@gmail.com>
2014-06-18 18:07:00 -0700

    add left children number and right children number

--- src/base/TreeNode.java
+++ src/base/TreeNode.java
@@ -6,11 +6,16 @@ public class TreeNode {
 	public TreeNode left;
 	public TreeNode right;
     public TreeNode parent;
+
+    public int leftChildrenNumber;
+    public int rightChildrenNumber;
 	
 	public TreeNode(int v) {
 		this.val = v;
 		this.left = null;
 		this.right = null;
         this.parent = null;
+        this.leftChildrenNumber = 0;
+        this.rightChildrenNumber = 0;
 	}
 }
