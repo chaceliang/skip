93991900aa9d0153bdb77e152bbbc9468efb22ad
Chace <chaceliang@gmail.com>
2014-06-19 17:55:31 -0700

    Reverse integer

--- src/edocteel/miscellaneous/ReverseInteger.java
+++ src/edocteel/miscellaneous/ReverseInteger.java
@@ -20,4 +20,30 @@ public class ReverseInteger {
         }
         return result;
     }
+
+    public static int reverse2(int x) {
+        boolean isNegative = false;
+        if (x < 0) {
+            isNegative = true;
+            x *= -1;
+        }
+        long result = 0;
+        while (x > 0) {
+            result = result * 10 + x % 10;
+            x /= 10;
+        }
+        if (isNegative) {
+            result *= -1;
+        }
+        if (result < Integer.MAX_VALUE && result > Integer.MIN_VALUE) {
+            return (int)result;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse2(23456789));
+        System.out.println(reverse2(456789));
+        System.out.println(reverse2(-123456789));
+    }
 }
