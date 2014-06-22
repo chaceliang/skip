ea50ac376351601d0ff0d4b8a23ac06eb2e9f8c2
Chace <chaceliang@gmail.com>
2014-06-22 12:11:24 -0700

    print all paths in binary tree

---
+++ src/tree/PrintAllPathsInBinaryTree.java
@@ -0,0 +1,37 @@
+package tree;
+
+import base.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * Created by chace on 6/22/14.
+ */
+public class PrintAllPathsInBinaryTree {
+    public static void printAllPaths(TreeNode root) {
+        printAllPathsUtil(root, new ArrayList<TreeNode>());
+    }
+
+    public static void printAllPathsUtil(TreeNode n, ArrayList<TreeNode> path) {
+        if (n == null) {
+            return;
+        }
+        path.add(n);
+        if (n.left == null && n.right == null) {
+            System.out.println(path);
+            path.remove(path.size() - 1);
+        } else {
+            printAllPathsUtil(n.left, path);
+            printAllPathsUtil(n.right, path);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(9);
+        root.right.right = new TreeNode(10);
+
+        printAllPaths(root);
+    }
+}
