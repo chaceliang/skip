93e1da27a6e8f1e200943631dd494443faa9807a
Chace <chaceliang@gmail.com>
2014-06-24 14:16:09 -0700

    Find all decode ways

--- src/edocteel/dp/DecodeWays.java
+++ src/edocteel/dp/DecodeWays.java
@@ -1,6 +1,7 @@
 package edocteel.dp;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 /**
  * Created by chace on 5/5/14.
@@ -59,5 +60,41 @@ public class DecodeWays {
                 }
             }
         }
+    }
+
+    public ArrayList<String> decodeWays(String s) {
+        Hashtable<String, String> mapping = new Hashtable<String, String>();
+        for (int i = 0; i < 26; i++) {
+            mapping.put(Integer.toString(i + 1), "" + (char)('A' + i));
+        }
+
+        ArrayList<String> results = new ArrayList<String>();
+        decodeUtil(s, "", results, mapping);
+        return results;
+    }
+
+    public void decodeUtil(String s, String result, ArrayList<String> results, Hashtable<String, String> mapping) {
+        if (s.length() == 0) {
+            results.add(result);
+        } else {
+            int value = s.charAt(0) - '0';
+            if (value >= 0 && value <= 9) {
+                decodeUtil(s.substring(1), result + mapping.get(s.substring(0, 1)), results, mapping);
+            }
+            if (s.length() >= 2) {
+                int v0 = s.charAt(0) - '0';
+                int v1 = s.charAt(1) - '0';
+                if (v0 == 1 && v1 >= 0 && v1 <= 9) {
+                    decodeUtil(s.substring(2), result + mapping.get(s.substring(0, 2)), results, mapping);
+                } else if (v0 == 2 && v1 >= 0 && v1 <= 6) {
+                    decodeUtil(s.substring(2), result + mapping.get(s.substring(0, 2)), results, mapping);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DecodeWays test = new DecodeWays();
+        System.out.println(test.decodeWays("123456"));
     }
 }
