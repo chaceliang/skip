fe3675afded5d6b2021dc2e8d28767694d6cdd2a
Chace <chaceliang@gmail.com>
2014-06-25 21:14:28 -0700

    add desc

--- src/edocteel/array/RemoveDuplicateFromSortedArray.java
+++ src/edocteel/array/RemoveDuplicateFromSortedArray.java
@@ -4,6 +4,10 @@ package edocteel.array;
  * Created by chace on 5/4/14.
  */
 public class RemoveDuplicateFromSortedArray {
+    /*
+    Given an array and a value, remove all instances of that value in place and return the new length.
+    It doesn't matter what you leave beyond the new length.
+     */
     public int removeDuplicates(int[] A) {
         if (A.length <= 1) {
             return A.length;
