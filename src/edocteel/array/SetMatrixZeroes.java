f5630477b3ccec2ac141053d5be82bbe5d079a76
Chace <chaceliang@gmail.com>
2014-06-25 22:14:51 -0700

    add desc

--- src/edocteel/array/SetMatrixZeroes.java
+++ src/edocteel/array/SetMatrixZeroes.java
@@ -4,6 +4,9 @@ package edocteel.array;
  * Created by chace on 5/4/14.
  */
 public class SetMatrixZeroes {
+    /*
+    Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+     */
     public void setZeroes(int[][] matrix) {
         boolean shouldSetFirstRow = false;
         boolean shouldSetFirstCol = false;
