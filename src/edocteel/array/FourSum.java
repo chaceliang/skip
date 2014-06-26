892506d076d7df3dea41cc639ff4370d83f3e371
Chace <chaceliang@gmail.com>
2014-06-25 20:34:05 -0700

    add desc

--- src/edocteel/array/FourSum.java
+++ src/edocteel/array/FourSum.java
@@ -7,10 +7,21 @@ import java.util.Iterator;
 
 public class FourSum {
 
+    /*
+    Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+    Note:
+    Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+    The solution set must not contain duplicate quadruplets.
+        For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+
+        A solution set is:
+        (-1,  0, 0, 1)
+        (-2, -1, 1, 2)
+        (-2,  0, 0, 2)
+     */
 	public static ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		
+
 		Arrays.sort(num);
 		ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
 		if (num.length < 4)
