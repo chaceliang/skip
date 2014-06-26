84b5f351fb6d6bb5fb30d0b52dbe91fc1bb3efcf
Chace <chaceliang@gmail.com>
2014-06-25 20:16:34 -0700

    add description

--- src/edocteel/array/FirstMissingPositiveNumber.java
+++ src/edocteel/array/FirstMissingPositiveNumber.java
@@ -2,6 +2,15 @@ package edocteel.array;
 
 public class FirstMissingPositiveNumber {
 
+    /*
+    Given an unsorted integer array, find the first missing positive integer.
+
+    For example,
+    Given [1,2,0] return 3,
+    and [3,4,-1,1] return 2.
+
+    Your algorithm should run in O(n) time and uses constant space.
+     */
 	public static int firstMissingPositive(int[] A) {
 		if (A == null || A.length <= 0) {
 			return 1;
@@ -26,7 +35,7 @@ public class FirstMissingPositiveNumber {
 		if (A[0] ==  A.length) {
 			return A.length + 1;
 		}
-		// Case: [0,1,2,3]
+		// Case: [0,1,2,3], [5,1,2,3]
 		return A.length;
 	}
 	/**
