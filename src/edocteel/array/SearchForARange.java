163cc7c13dd1c02d8aba6d4450cc0e9952fc1a73
Chace <chaceliang@gmail.com>
2014-06-25 22:03:26 -0700

    add desc

--- src/edocteel/array/SearchForARange.java
+++ src/edocteel/array/SearchForARange.java
@@ -2,6 +2,17 @@ package edocteel.array;
 
 public class SearchForARange {
 
+    /*
+    Given a sorted array of integers, find the starting and ending position of a given target value.
+
+    Your algorithm's runtime complexity must be in the order of O(log n).
+
+    If the target is not found in the array, return [-1, -1].
+
+    For example,
+    Given [5, 7, 7, 8, 8, 10] and target value 8,
+    return [3, 4].
+     */
 	public int[] searchRange(int[] A, int target) {
         int start = -1, end = -1;
         if (A == null || A.length <= 0) {
