776a4f7e9a2dd184aa1331cad15f0f678f925565
Chace <chaceliang@gmail.com>
2014-06-25 20:59:22 -0700

    add desc

--- src/edocteel/array/MedianOfTwoArray.java
+++ src/edocteel/array/MedianOfTwoArray.java
@@ -5,6 +5,11 @@ package edocteel.array;
  */
 public class MedianOfTwoArray {
 
+    /*
+    There are two sorted arrays A and B of size m and n respectively.
+    Find the median of the two sorted arrays.
+    The overall run time complexity should be O(log (m+n)).
+     */
     public double findMedianSortedArrays(int A[], int B[]) {
         int m = A.length;
         int n = B.length;
