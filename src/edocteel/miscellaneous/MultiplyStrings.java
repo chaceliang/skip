214205be9051f712506b15f025c4ffec2863b85d
Chace <chaceliang@gmail.com>
2014-06-28 10:15:58 -0700

    another way

--- src/edocteel/miscellaneous/MultiplyStrings.java
+++ src/edocteel/miscellaneous/MultiplyStrings.java
@@ -47,4 +47,37 @@ public class MultiplyStrings {
         }
         return sb.toString();
     }
+
+    public String multiply2(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int l1 = num1.length(), l2 = num2.length();
+        int[] n1 = new int[l1];
+        int[] n2 = new int[l2];
+
+        for (int i = 0; i < l1; i++) {
+            n1[l1 - i - 1] = num1.charAt(i) - '0';
+        }
+        for (int i = 0; i < l2; i++) {
+            n2[l2 - i - 1] = num2.charAt(i) - '0';
+        }
+
+        int sum = 0;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < l1+l2-1; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j < l1 && i - j < l2) {
+                    sum += n1[j] * n2[i-j];
+                }
+            }
+            builder.append((char)(sum % 10 + '0'));
+            sum /= 10;
+        }
+        if (sum > 0) {
+            builder.append((char)(sum + '0'));
+        }
+        String s = builder.reverse().toString();
+        return s.isEmpty() ? "0" : s;
+    }
 }
