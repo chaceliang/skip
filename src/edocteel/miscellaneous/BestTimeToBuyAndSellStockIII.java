3a0478bac20f18fadfe459a56598a223a3edde05
Chace <chaceliang@gmail.com>
2014-06-27 22:22:12 -0700

    clean up code

--- src/edocteel/miscellaneous/BestTimeToBuyAndSellStockIII.java
+++ src/edocteel/miscellaneous/BestTimeToBuyAndSellStockIII.java
@@ -17,13 +17,13 @@ public class BestTimeToBuyAndSellStockIII {
             min = prices[i] < min ? prices[i] : min;
         }
         int max = prices[length - 1];
-        for (int i = length - 2; i >= 0; i--) {
-            r[i] = max - prices[i] > r[i + 1] ? max - prices[i] : r[i + 1];
-            max = prices[i] > max ? prices[i] : max;
-        }
         int profit = 0;
-        for (int i = 0; i < length; i++) {
-            profit = l[i] + r[i] > profit ? l[i] + r[i] : profit;
+        for (int i = length - 1; i >= 0; i--) {
+            if (i < length - 1) {
+                r[i] = max - prices[i] > r[i + 1] ? max - prices[i] : r[i + 1];
+                max = prices[i] > max ? prices[i] : max;
+            }
+            profit = Math.max(l[i] + r[i], profit);
         }
         return profit;
     }
