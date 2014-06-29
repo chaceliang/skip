236987e61035d9b300a3bd0b75a79680759e0f11
Chace <chaceliang@gmail.com>
2014-06-29 14:48:56 -0700

    add decompresor

--- src/string/StringCompressor.java
+++ src/string/StringCompressor.java
@@ -33,7 +33,35 @@ public class StringCompressor {
         return builder.toString();
     }
 
+    public static String decompressor(String str) {
+        if (str == null) {
+            throw new NullPointerException();
+        }
+        StringBuilder builder = new StringBuilder();
+        int i = 0, len = str.length();
+        while (i < len) {
+            if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                if (i < len - 2 && str.charAt(i+1) == '*') {
+                    int times = str.charAt(i) - '0';
+                    char c = str.charAt(i+2);
+                    while (times-- > 0) {
+                        builder.append(c);
+                    }
+                    i += 2;
+                } else {
+                    builder.append(str.charAt(i));
+                }
+            } else {
+                builder.append(str.charAt(i));
+            }
+            i++;
+        }
+        return builder.toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println(compressor("123abkkkkkc"));
+        System.out.println("1223abkkkkkc");
+        System.out.println(compressor("1223abkkkkkc*"));
+        System.out.println(decompressor(compressor("1223abkkkkkc1*")));
     }
 }
