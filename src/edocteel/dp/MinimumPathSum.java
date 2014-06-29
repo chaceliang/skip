74981c3bf2871342c0caaeb07fa9baa915d31208
Chace <chaceliang@gmail.com>
2014-06-28 19:53:12 -0700

    start from 00

--- src/edocteel/dp/MinimumPathSum.java
+++ src/edocteel/dp/MinimumPathSum.java
@@ -25,6 +25,24 @@ public class MinimumPathSum {
         return matrix[0][0];
     }
 
+    public static int minPathSum2(int[][] grid) {
+        int rL = grid.length;
+        int cL = grid[0].length;
+        int[][] matrix = new int[rL][cL];
+        matrix[0][0] = grid[0][0];
+        for (int i = 1; i < rL; i++) {
+            matrix[i][0] = matrix[i-1][0] + grid[i][0];
+        }
+        for (int i = 1; i < cL; i++) {
+            matrix[0][i] = matrix[0][i-1] + grid[0][i];
+        }
+        for (int i = 1; i < rL; i++) {
+            for (int j = 1; j < cL; j++) {
+                matrix[i][j] = Math.min(matrix[i-1][j], matrix[i][j-1]) + grid[i][j];
+            }
+        }
+        return matrix[rL-1][cL-1];
+    }
     public static void main(String[] args) {
         minPathSum(new int[][]{{1,2,5},{3,2,1}});
     }
