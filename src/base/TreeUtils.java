d7bc9c150f70ae04fb9757bafb2239307471be11
Chace <chaceliang@gmail.com>
2014-07-02 14:08:58 -0700

    add print by level

--- src/base/TreeUtils.java
+++ src/base/TreeUtils.java
@@ -1,9 +1,37 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeUtils {
 
 	public static void printTreeByLevel(TreeNode root) {
-		
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> tmp = new LinkedList<TreeNode>();
+        tmp.add(root);
+        while (!tmp.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            int size = tmp.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = tmp.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    tmp.add(node.left);
+                }
+                if (node.right != null) {
+                    tmp.add(node.right);
+                }
+            }
+            result.add(level);
+        }
+        for (ArrayList<Integer> al : result) {
+            System.out.println(al);
+        }
+        return;
 	}
 	
 	public static void printTreeInorder(TreeNode node) {
