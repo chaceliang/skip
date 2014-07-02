5341125133f5bfd57f2bb64aac9bb42ee80c2db8
Chace <chaceliang@gmail.com>
2014-07-02 14:08:47 -0700

    refactor

--- src/tree/SinkZerosInBinaryTree.java
+++ src/tree/SinkZerosInBinaryTree.java
@@ -1,26 +1,36 @@
 package tree;
 
 import base.TreeNode;
+import base.TreeUtils;
+import base.Utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
  * Created by chace on 6/28/14.
  */
 public class SinkZerosInBinaryTree {
-    public static void sink0(TreeNode node, Stack<TreeNode> stack) {
+    public static void sink0(TreeNode node, Queue<TreeNode> queue) {
         if (node.val == 0) {
-            stack.push(node);
-        }
-        if (node.left != null) {
-            sink0(node.left, stack);
-        }
-        if (node.right != null) {
-            sink0(node.right, stack);
+            queue.offer(node);
+        } else if (queue.size() > 0){
+            TreeNode zero = queue.poll();
+            zero.val = node.val;
+            node.val = 0;
+            queue.offer(node);
         }
 
-        if (stack.size() > 0) {
-            TreeNode zero = stack.pop();
+        if (node.left != null) {
+            sink0(node.left, queue);
+        }
+        if (node.right != null) {
+            sink0(node.right, queue);
+        }
+
+        if (queue.size() > 0) {
+            TreeNode zero = queue.poll();
             zero.val = node.val;
             node.val = 0;
         }
@@ -41,5 +51,18 @@ public class SinkZerosInBinaryTree {
         if (node.right != null) {
             sink02(node.right, stack);
         }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        root.right = new TreeNode(0);
+        root.right.left = new TreeNode(3);
+
+        TreeUtils.printTreeByLevel(root);
+
+        sink0(root, new LinkedList<TreeNode>());
+        TreeUtils.printTreeByLevel(root);
     }
 }
