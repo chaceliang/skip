07ba965dea025c6dce77452b6d0f2a35fa1910a9
chaceliang <chaceliang@gmail.com>
2014-07-20 12:57:56 -0700

    Edge case tweak

--- src/string/StringCompressor.java
+++ src/string/StringCompressor.java
@@ -4,55 +4,56 @@ package string;
  * Created by chace on 6/6/14.
  */
 public class StringCompressor {
-    // 123abkkkkkc => 123ab5xkc
 
-    public static String compressor(String str) {
-        if (str == null || str.length() <= 0) {
-            return str;
-        }
-        int i = 0, len = str.length();
+    // Google onsite.
+    // String compressor that turns "123abkkkkkc" to "123ab5xkc".
+    // Decompressor is already written and must remain unchanged.
+    // take into account of strings like: "123xk", "123xx" ...etc as input
+
+    public static String compressor(String s) {
         StringBuilder builder = new StringBuilder();
+
+        int i = 0, len = s.length();
         while (i < len) {
             int times = 1;
-            int j = i + 1;
+            int j = i+1;
             while (j < len) {
-                if (str.charAt(j) == str.charAt(i)) {
-                    j++;
-                    times++;
+                if (s.charAt(i) == s.charAt(j)) {
+                    j++; times++;
                 } else {
                     break;
                 }
             }
-            if (times > 1) {
-                builder.append(times);
-                builder.append("*");
+            if (s.charAt(i) == '*' && i != len-1 && times == 1) {
+                builder.append("1**");
+            } else {
+                if (times > 1) {
+                    builder.append(times);
+                    builder.append("*");
+                }
+                builder.append(s.charAt(i));
             }
-            builder.append(str.charAt(i));
             i = j;
         }
+
         return builder.toString();
     }
 
-    public static String decompressor(String str) {
-        if (str == null) {
-            throw new NullPointerException();
-        }
+    public static String decompressor(String s) {
         StringBuilder builder = new StringBuilder();
-        int i = 0, len = str.length();
+        int i = 0, len = s.length();
         while (i < len) {
-            if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
-                if (i < len - 2 && str.charAt(i+1) == '*') {
-                    int times = str.charAt(i) - '0';
-                    char c = str.charAt(i+2);
-                    while (times-- > 0) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                if (i < len - 2 && s.charAt(i+1) == '*') {
+                    int times = s.charAt(i) - '0';
+                    char c = s.charAt(i+2);
+                    while (times-- >= 0) {
                         builder.append(c);
                     }
                     i += 2;
-                } else {
-                    builder.append(str.charAt(i));
                 }
             } else {
-                builder.append(str.charAt(i));
+                builder.append(s.charAt(i));
             }
             i++;
         }
@@ -61,7 +62,7 @@ public class StringCompressor {
 
     public static void main(String[] args) {
         System.out.println("1223abkkkkkc");
-        System.out.println(compressor("1223abkkkkkc*"));
+        System.out.println(compressor("1223abkkkkk*c*"));
         System.out.println(decompressor(compressor("1223abkkkkkc1*")));
     }
 }
