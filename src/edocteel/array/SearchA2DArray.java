6b329693cd42c656943159cf88b4ce2dea05aea7
chaceliang <chaceliang@gmail.com>
2014-07-22 20:06:11 -0700

    lala

--- src/edocteel/array/SearchA2DArray.java
+++ src/edocteel/array/SearchA2DArray.java
@@ -23,6 +23,21 @@ public class SearchA2DArray {
 		}
 		return false;
 	}
+
+    // 每行元素不一定都比前一行大
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
 	
 	public boolean searchMatrix(int[][] matrix, int target) {
 		if (matrix.length == 0 || matrix[0].length == 0) {
